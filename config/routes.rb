42063d7ca920b980e27d108307233a25cb0c1153
TakumiNakamura <155757338+TAKUMI120108@users.noreply.github.com>
2024-03-20 16:31:45 +0000

    [Finish]meshiterro

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,21 @@
 Rails.application.routes.draw do
+  devise_for :admin, skip: [:registrations, :passward], controllers: {
+    sessions: 'admin/sessions'
+  }
+  
+  namespace :admin do
+    get 'dashboards', to: 'dashboards#index'
+    resources :users, only:[:destroy]
+  end
+  
+  scope module: :public do
   devise_for :users
   root to: 'homes#top'
-  get "homes/about" => "homes#about", as: "about"
+  get "homes/about", to: 'homes#about', as: :about
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-  resource  :favorite, only:[:create, :destroy]
-  resources :post_comments, only: [:create, :destroy]
+    resource  :favorite, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
   end
+end
